4bc8c75b092e890e01ce7672aaf629d3875764b1
Mauricio Hernández Cabrera <mauriciohernandez9487@outlook.com>
2025-11-13 12:41:35 -0500

    primer commit, junto con visualizacion de Swagger

---
+++ SistemaEventos_v2/SistemaEventos/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Agregar servicios de Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configuración de Swagger para habilitar middleware q lanza la interfaz de usuario de Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
